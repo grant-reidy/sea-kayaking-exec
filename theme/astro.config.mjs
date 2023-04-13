b3cd9b03d3c4ff96a22c5bef1476e9ef16698ee9
greidy3 <grant.reidy@gatech.edu>
2023-04-12 21:04:08 -0400

    Created Site
    New website for sea-kayaking exec

---
+++ theme/astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+import lit from "@astrojs/lit";
+import partytown from "@astrojs/partytown";
+
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://seakayakgt.org/',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  sitemap: true,
+  // Generate sitemap (set to "false" to disable)
+  integrations: [sitemap(), mdx(), image(), lit(), partytown({config: {forward: ["dataLayer.push"]}})],
+  // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
---
+++ theme/src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Sea Kayaking`,
+	description: `The hub for information about Sea Kayaking at Georgia Tech.`,
+	url: `https://seakayakgt.org`, // No trailing slash!
+	name: `Sea Kayaking`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: false,
+  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
