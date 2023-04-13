810ef9d7b61b9c40a7a940c3a956de60be64efeb
greidy3 <grant.reidy@gatech.edu>
2023-04-12 21:27:41 -0400

    Updated

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -4,75 +4,12 @@ export const footerSocials = [
     url: 'https://groupme.com/join_group/65553889/bgZHyFiA',
     icon: "mdi:forum",
   },
-//   {
-//     name: 'Slack',
-//     url: 'https://orgtworkspace.slack.com',
-//     icon: "mdi:slack",
-//   },
+  {
+    name: 'Slack',
+    url: 'https://orgtworkspace.slack.com',
+    icon: "mdi:slack",
+  },
 ]
 
 export const footerLists = [
-	{
-		title: 'IP Trips',
-		items: [
-			{
-				title: 'North Carolina Lakes',
-				slug: '/ip-trips/nc-lakes',
-			},
-			{
-				title: 'Okefenokee',
-				slug: '/ip-trips/okefenokee',
-			},
-			{
-				title: 'Lake Jocassee',
-				slug: '/ip-trips/lake-jocassee',
-			},
-			{
-				title: 'Tybee Island',
-				slug: '/ip-trips/tybee-island',
-			},
-		],
-	},
-	{
-		title: 'Expeditions',
-		items: [
-			{
-				title: 'Antarctica 2023',
-				slug: '/expeditions/antarctica-2023',
-			},
-			{
-				title: 'Greenland',
-				slug: '/expeditions/greenland',
-			},
-			{
-				title: 'British Columbia',
-				slug: '/expedtiions/british-columbia',
-			},
-			{
-				title: 'Chile 2019',
-				slug: '/expedtiions/chile-2019',
-			},
-			{
-				title: 'Antarctica 2018',
-				slug: '/expeditions/antarctica-2018'
-			},
-			{
-				title: 'Costa Rica',
-				slug: '/expeditions/costa-rica'
-			}
-		],
-	},
-	{
-		title: 'About',
-		items: [
-			{
-				title: 'Meetings',
-				slug: '/about/meetings',
-			},
-			{
-				title: 'Executive Board',
-				slug: '/about',
-			},
-		],
-	},
 ];
--- theme/src/config/nav.js
+++ theme/src/config/nav.js
@@ -1,18 +1,2 @@
 export const nav = [
-	{
-		title: 'Home',
-		slug: '/',
-	},
-	{
-		title: 'IP Trips',
-		slug: '/ip-trips',
-	},
-	{
-		title: 'Expeditions',
-		slug: '/expeditions',
-	},
-	{
-		title: 'About',
-		slug: '/about',
-	},
 ];
