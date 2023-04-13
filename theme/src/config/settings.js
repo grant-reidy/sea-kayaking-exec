c7e30c62edfc8ab6a82e4a052d84209ee613bb15
greidy3 <grant.reidy@gatech.edu>
2023-04-12 21:32:13 -0400

    Removed Analytics

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Sea Kayaking`,
-	description: `The hub for information about Sea Kayaking at Georgia Tech.`,
-	url: `https://seakayakgt.org`, // No trailing slash!
+	description: `The hub for executive information about Sea Kayaking at Georgia Tech.`,
+	url: `https://sea-kayaking-exec.netlify.app`, // No trailing slash!
 	name: `Sea Kayaking`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
